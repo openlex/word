9c1a49e64aa9e5547fdaabaeb1ed26d70ab45e79
a.kozeruk <openlexx@gmail.com>
2020-10-19 22:16:39 +0300

    small fix in store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -4,13 +4,6 @@ import { userReducer } from "./user";
 
 const middleware = [thunkMiddleware];
 
-if (process.env.NODE_ENV === `development`) {
-	// eslint-disable-next-line @typescript-eslint/no-var-requires
-	const { logger } = require(`redux-logger`);
-
-	middleware.push(logger);
-}
-
 export const store = configureStore({
 	reducer: {
 		userReducer,
